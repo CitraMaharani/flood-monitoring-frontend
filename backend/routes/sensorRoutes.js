9e7143c27f53b6b27b0f4f0c819d051ddad5d0a0
citramhrn <102406034+CitraMaharani@users.noreply.github.com>
2026-02-02 18:52:35 +0700

    Delete backend directory

--- backend/routes/sensorRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const sensor = require("../controllers/sensorController");
-const auth = require("../middleware/auth");
-
-router.get("/", auth, sensor.list);
-
-module.exports = router;
